cb636be54ca540eda236c4c3a103a3d60f1c0a15
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-11-11 12:42:42 +0100

    Changed name of variable

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const subNavigation = document.querySelector('.sub-navigation');
 const menuButton = document.querySelector('.main-navigation__menu-button');
 const moreButton = document.querySelector('.main-navigation__more-button');
 
-const logoNavigation = document.querySelector('.main-navigation__logo');
+const navigationLogo = document.querySelector('.main-navigation__logo');
 const videoBody = document.querySelector('.video-section__thumbnail');
 
 // Menu button
@@ -43,7 +43,7 @@ function handleWindowResize() {
 let isMainMenuOpen = false;
 function toggleMainMenu() {
 	navigation.classList.toggle('navigation--open');
-	logoNavigation.classList.toggle('main-navigation__logo--open');
+	navigationLogo.classList.toggle('main-navigation__logo--open');
 	menuButton.classList.toggle('main-navigation__menu-button--open');
 
 	isMainMenuOpen = !isMainMenuOpen;
@@ -91,7 +91,7 @@ function resetMobileMainNavigation() {
 	// if greater than 1100 px
 	if (window.innerWidth > 1100 && isMainMenuOpen === true){
 		navigation.classList.remove('navigation--open');
-		logoNavigation.classList.remove('main-navigation__logo--open');
+		navigationLogo.classList.remove('main-navigation__logo--open');
 		menuButton.classList.remove('main-navigation__menu-button--open');
 	}
 }
