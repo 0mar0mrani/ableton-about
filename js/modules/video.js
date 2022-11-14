ae31b4db8fcbd245cefa47dfdb8b6817709e2b62
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-11-14 14:21:06 +0100

    Added comments

--- js/modules/video.js
+++ js/modules/video.js
@@ -1,12 +1,16 @@
 export default function Video() {
+	// Query Selector
 	const videoBody = document.querySelector('.video-section__thumbnail');
 
+	// Event Listener
 	videoBody.addEventListener('click', handleVideoBodyClick);
 
+	// Handlers
 	function handleVideoBodyClick() {
 		hideVideoThumbnail();
 	}
 	
+	// Methods
 	function hideVideoThumbnail() {
 		videoBody.classList.add('video-section__thumbnail--hidden');
 	}
